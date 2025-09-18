876208e7f3e5ccd25a4f00be7a5bf600345b30ee
wenn1107 <wenzhang8028@gmail.com>
2025-09-18 15:25:50 +0800

    Create main2.c

---
+++ ex2/main2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char a = 5;
+    printf("\t%d\n", a^1);
+    printf("\t%d\n", ~a);
+    printf("\t%d\n", a>>1);
+    printf("\t%d\n", a<<1);
+    return 0;
+}
