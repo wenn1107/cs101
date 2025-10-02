ce92a677390614948ead577aa95196e54922a5f2
wenn1107 <wenzhang8028@gmail.com>
2025-10-02 19:42:17 +0800

    Create main3.c

---
+++ ex3/main3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    if (i % 15 == 0){
+        printf("Love IU");
+    }
+    else if (i % 3 == 0){
+        printf("Love");
+    }
+    else if (i % 5 == 0){
+        printf("IU");
+    }
+    else {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
